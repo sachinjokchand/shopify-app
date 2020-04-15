733d4c23e1c5e2587cc4d5b7cc4231def98e2790
sachin jogchand <sachincodingkart@gmail.com>
2020-04-15 20:20:31 +0530

    initial commit

--- index.js
+++ index.js
@@ -153,7 +153,9 @@ app.get('/shopify/callback', (req, res) => {
                    // console.log( obj );
 
                    // app.use('/', express.static('./node_modules/admin-lte'));
-                  res.render(express.static('./node_modules/admin-lte') ,{ shop_data : shop_data });
+                   app.use('/admin', express.static('./node_modules/admin-lte-express/public'))
+                   app.use('/', require('admin-lte-express'));
+                  // res.render(express.static('./node_modules/admin-lte') ,{ shop_data : shop_data });
                 } 
              else {
                   // res.render('home',{ shop_data : err });
