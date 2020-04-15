bf48d49e5e10f801e9fba383ab83dd56fdd4c3ed
sachin jogchand <sachincodingkart@gmail.com>
2020-04-15 21:35:25 +0530

    initial commit

--- index.js
+++ index.js
@@ -204,5 +204,5 @@ app.post('/add-to-wish',(req, res) => {
 });
 
 app.get('/dashboard',(req, res) => {  
-  res.render('dashboard' , );
+  res.render('dashboard' ,{ "shop_data" : shop_data } );
 });
