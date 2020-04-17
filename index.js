ccc070c7ca56d7ad724d3718b767d91122031054
sachin jogchand <sachincodingkart@gmail.com>
2020-04-17 16:06:40 +0530

    initial commit

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const conn = new Pool({
 // 'CREATE TABLE user_data(id SERIAL PRIMARY KEY, customer_id VARCHAR(255) not null, customer_name VARCHAR(255), customer_email VARCHAR(255), shop_name VARCHAR(255) not null)');
 
 conn.query(
-'CREATE TABLE product_data(id SERIAL PRIMARY KEY, shop_name VARCHAR(255) not null, customer_id VARCHAR(255), product_id VARCHAR(255) not null, product_title VARCHAR(255) not null, product_src VARCHAR(255) not null, product_url VARCHAR(255) not null, product_id VARCHAR(255) not null)');
+'CREATE TABLE product_data(id SERIAL PRIMARY KEY, shop_name VARCHAR(255) not null, customer_id VARCHAR(255), product_id VARCHAR(255) not null, product_title VARCHAR(255) not null, product_src VARCHAR(255) not null, product_url VARCHAR(255) not null)');
 
 // conn.query(
 // 'CREATE TABLE wish_list(id SERIAL PRIMARY KEY, shop_name VARCHAR(255) not null, customer_id VARCHAR(255))');
