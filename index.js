82db20289007e60a5abc3f905ddcaa10b24ff091
sachin jogchand <sachincodingkart@gmail.com>
2020-04-22 10:57:02 +0530

    initial commit

--- index.js
+++ index.js
@@ -146,13 +146,13 @@ app.get('/shopify/callback', (req, res) => {
       // request.get(shopRequestUrl, { headers: shopRequestHeaders })
       // .then((shopResponse) => {
             var shop_data = {};
-            let sql_user = "SELECT * FROM user_data WHERE shop_name='"+shop+"'";
+            let sql_user = "SELECT * FROM user_data WHERE shop_name='"+shop+"' ORDER BY id DESC";
             let query_user = conn.query(sql_user, (err, results) => {
               // console.log(results);
              if (results.rows.length>0) 
                 {
                    shop_data['user_data'] =  results.rows;
-                   let sql_pro = "SELECT * FROM product_data WHERE shop_name='"+shop+"'";
+                   let sql_pro = "SELECT * FROM product_data WHERE shop_name='"+shop+"' ORDER BY id DESC,";
                     let query_pro = conn.query(sql_pro, (err, results) => {
                       // console.log(results);
                      if (results.rows.length>0) 
