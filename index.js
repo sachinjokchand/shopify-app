5167fe3fc5c7a4575d979ee616151a845d0b02e7
sachin jogchand <sachincodingkart@gmail.com>
2020-04-25 18:33:38 +0530

    initail commit

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ app.get('/shopify/callback', (req, res) => {
          //  });
 //*************************get assets end***************************************
 //*************************get specific file start******************************
-        const asetsFileUrl ='https://' + shop + '/admin/api/2020-04/themes/'+themeid+'/assets.json?asset[key]=templates/index.liquid';
+        const asetsFileUrl ='https://' + shop + '/admin/api/2020-04/themes/'+themeid+'/assets.json?asset[key]=templates/index1.liquid';
            request.get(asetsFileUrl, { headers: asetsheader})
           .then(function (response) {
                  const parsedResponce = JSON.parse(response);
@@ -189,7 +189,7 @@ app.get('/shopify/callback', (req, res) => {
 
      let add_assets_asset = {
                     "asset": {
-                      "key": "templates/index.liquid",
+                      "key": "templates/index1.liquid",
                      "value": filedata
                     }
                 };
