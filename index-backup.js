a58330f49ecb0589a8e99627d7326bb4b6b78aa2
sachin jogchand <sachincodingkart@gmail.com>
2020-04-25 13:05:47 +0530

    initail commit

--- index-backup.js
+++ index-backup.js
@@ -93,33 +93,29 @@ app.get('/shopify/callback', (req, res) => {
       const accessToken = accessTokenResponse.access_token;
       // DONE: Use access token to make API call to 'script_tags' endpoint
       // const createScriptTagUrl = 'https://' + shop + '/admin/script_tags.json';
-      const shopRequestHeaders = {
-                'X-Shopify-Access-Token': accessToken
-            };
-            
-      const createScriptTagUrl = 'https://' + shop + '/admin/script_tags.json';
-      // const createScriptTagUrl = 'https://shopname.myshopify.com/admin/script_tags.json';
-      const scriptTagBody = {
-                "script_tag": {
-                    "event": "onload",
-                    "src": "https:\/\/digitalcodingkloud.000webhostapp.com\/app_js_functions.js"
-
+      var shop_data = {};
+              let sql_user = "SELECT * FROM user_data WHERE shop_name='"+shop+"' ORDER BY id DESC";
+              let query_user = conn.query(sql_user, (err, results) => {
+                // console.log(results);
+               if (results.rows.length>0) 
+                  {
+                     shop_data['user_data'] =  results.rows;
+                     let sql_pro = "SELECT * FROM product_data WHERE shop_name='"+shop+"' ORDER BY id DESC";
+                      let query_pro = conn.query(sql_pro, (err, results) => {
+                        // console.log(results);
+                       if (results.rows.length>0) 
+                          {  
+                            shop_data['product_data'] =  results.rows;
+                            shop_data['current_time'] = new Date().toISOString();
+                            res.render('index' ,{ shop_data : shop_data });
+                          } 
+                   });
                 }
-            }
-        request.post({
-            url: createScriptTagUrl,
-            body: scriptTagBody,
-            headers: shopRequestHeaders,
-            json: true
-        }, function (error, response, body) {
-         
-         if (!error) {
-         	res.send('Hello World!');
-          console.log(body)
-             }
-            //Do whatever you want with the body
-        });
-    })
+               else {
+                    // res.render('home',{ shop_data : err });
+                   }
+             });
+      })
     .catch((error) => {
       res.status(error.statusCode).send(error.error.error_description);
     });
@@ -128,6 +124,97 @@ app.get('/shopify/callback', (req, res) => {
     // Validate request is from Shopify
     // Exchange temporary code for a permanent access token
       // Use access token to make API call to 'shop' endpoint
+  } else {
+    res.status(400).send('Required parameters missing');
+  }
+});
+
+app.get('/shopify/callback', (req, res) => {
+  const { shop, hmac, code, state } = req.query;
+  const stateCookie = cookie.parse(req.headers.cookie).state;
+
+  if (state !== stateCookie) {
+    return res.status(403).send('Request origin cannot be verified');
+  }
+
+  if (shop && hmac && code) {
+    // DONE: Validate request is from Shopify
+    const map = Object.assign({}, req.query);
+    delete map['signature'];
+    delete map['hmac'];
+    const message = querystring.stringify(map);
+    const providedHmac = Buffer.from(hmac, 'utf-8');
+    const generatedHash = Buffer.from(
+      crypto
+        .createHmac('sha256', apiSecret)
+        .update(message)
+        .digest('hex'),
+        'utf-8'
+      );
+    let hashEquals = false;
+
+    try {
+      hashEquals = crypto.timingSafeEqual(generatedHash, providedHmac)
+    } catch (e) {
+      hashEquals = false;
+    };
+
+    if (!hashEquals) {
+      return res.status(400).send('HMAC validation failed');
+    }
+
+    // DONE: Exchange temporary code for a permanent access token
+    const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    const accessTokenPayload = {
+      client_id: apiKey,
+      client_secret: apiSecret,
+      code,
+    };
+
+    request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+    .then((accessTokenResponse) => {
+      const accessToken = accessTokenResponse.access_token;
+      // DONE: Use access token to make API call to 'shop' endpoint
+      // const shopRequestUrl = 'https://' + shop + '/admin/api/2020-04/products.json';
+      // const shopRequestHeaders = {
+      //   'X-Shopify-Access-Token': accessToken,
+      // }; 
+      
+      // res.render('home',{ shop_data : "hello sachin" });
+      // request.get(shopRequestUrl, { headers: shopRequestHeaders })
+      // .then((shopResponse) => {
+            var shop_data = {};
+            let sql_user = "SELECT * FROM user_data WHERE shop_name='"+shop+"' ORDER BY id DESC";
+            let query_user = conn.query(sql_user, (err, results) => {
+              // console.log(results);
+             if (results.rows.length>0) 
+                {
+                   shop_data['user_data'] =  results.rows;
+                   let sql_pro = "SELECT * FROM product_data WHERE shop_name='"+shop+"' ORDER BY id DESC";
+                    let query_pro = conn.query(sql_pro, (err, results) => {
+                      // console.log(results);
+                     if (results.rows.length>0) 
+                        {  
+                          shop_data['product_data'] =  results.rows;
+                          shop_data['current_time'] = new Date().toISOString();
+                          res.render('index' ,{ shop_data : shop_data });
+                        } 
+                 });
+              }
+             else {
+                  // res.render('home',{ shop_data : err });
+                 }
+           });
+        // res.status(200).end(shopResponse);
+      // })
+      // .catch((error) => {
+      //   res.status(error.statusCode).send(error.error.error_description);
+      // });
+    })
+    .catch((error) => {
+      res.status(error.statusCode).send(error.error.error_description);
+    });
+
   } else {
     res.status(400).send('Required parameters missing');
   }
