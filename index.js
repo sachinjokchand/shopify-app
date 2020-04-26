75af71d675ed9471890426450b8010ee46e57a55
sachin jogchand <sachincodingkart@gmail.com>
2020-04-26 22:44:23 +0530

    initail commit

--- index.js
+++ index.js
@@ -159,8 +159,8 @@ app.get('/shopify/callback', (req, res) => {
 
                let add_assets_asset = {
                                 "asset": {
-                                "key": "layout/alternate.liquid",
-                                "source_key": "layout/theme.liquid"
+                                "key": "layout/alternate-new.liquid",
+                                "source_key": "https://digitalcodingkloud.000webhostapp.com//alternate.liquid"
                               }
                           };
                let assests_optionssss = {
