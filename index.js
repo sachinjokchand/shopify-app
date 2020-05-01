d21b6dd1ccc297a24b7228225bec52e78ea3d057
sachin jogchand <sachincodingkart@gmail.com>
2020-05-02 00:26:54 +0530

    initail commit

--- index.js
+++ index.js
@@ -393,6 +393,7 @@ app.post('/remove_prod',(req, res) => {
 
 app.post('/get_wish_list',(req, res) => {  
    var pro_details_arr = [];
+   var i  = 0;
    var pro_details     = {};
    var pro_arr         = req.body.pro_arr;
    var shop_name       =  req.body.shop_name;
@@ -409,7 +410,7 @@ app.post('/get_wish_list',(req, res) => {
     }
    else
    {
-    for (var i = 0; i < pro_arr.length; i++) {
+    for ( i = 0; i < pro_arr.length; i++) {
        pro_details['product_id']    = pro_arr[i];
        pro_details_arr[i]  = pro_details;
     //   const shopRequestUrl_prod = 'https://' + req.body.shop_name + '/admin/api/2020-04/products/'+pro_arr[i]+'.json';
@@ -434,7 +435,7 @@ app.post('/get_wish_list',(req, res) => {
     //   });   
     }  
     // console.log(pro_details);
-    res.send(pro_details);
+    res.send(pro_details_arr);
    } 
 });
 
