7b0588b412879d7a993784dfdcbda835142b6022
sachin jogchand <sachincodingkart@gmail.com>
2020-05-02 10:35:28 +0530

    initail commit

--- index.js
+++ index.js
@@ -414,27 +414,28 @@ app.post('/get_wish_list',(req, res) => {
     for ( i = 0; i < pro_arr.length; i++) {
 
       const shopRequestUrl_prod = 'https://' + req.body.shop_name + '/admin/api/2020-04/products/'+pro_arr[i]+'.json';
-      const shopRequestHeaders_prod = {
-        'X-Shopify-Access-Token': accessToken,
-      };
+      res.send(shopRequestUrl_prod);
+      // const shopRequestHeaders_prod = {
+      //   'X-Shopify-Access-Token': accessToken,
+      // };
 
-      global_req.get(shopRequestUrl_prod, { headers: shopRequestHeaders_prod })
-      .then((shopResponse) => {
-       shop_resp = JSON.parse(shopResponse);
-       pro_details_arr[i]  = shop_resp;
-       pro_details_arr["ssss"]  = "4444444";
-       res.send(pro_details_arr);
-       //  var url        = shop_resp.product.title.replace(/\s+/g, '-').toLowerCase();
-       //  var pro_url    = 'https://' + req.body.shop_name+'/products/'+url;
+      // global_req.get(shopRequestUrl_prod, { headers: shopRequestHeaders_prod })
+      // .then((shopResponse) => {
+      //  shop_resp = JSON.parse(shopResponse);
+      //  pro_details_arr[i]  = shop_resp;
+      //  pro_details_arr["ssss"]  = "4444444";
+      //  res.send(pro_details_arr);
+      //  //  var url        = shop_resp.product.title.replace(/\s+/g, '-').toLowerCase();
+      //  //  var pro_url    = 'https://' + req.body.shop_name+'/products/'+url;
 
-       // pro_details_arr[i]['product_id']    = shop_resp.product.id;
-       // pro_details_arr[i]['product_src']   = shop_resp.product.image.src;
-       // pro_details_arr[i]['product_url']   =  pro_url; 
-       // pro_details_arr[i]['product_title'] = shop_resp.product.title;
-       })
-      .catch((error) => {
-        res.send(error);
-      });   
+      //  // pro_details_arr[i]['product_id']    = shop_resp.product.id;
+      //  // pro_details_arr[i]['product_src']   = shop_resp.product.image.src;
+      //  // pro_details_arr[i]['product_url']   =  pro_url; 
+      //  // pro_details_arr[i]['product_title'] = shop_resp.product.title;
+      //  })
+      // .catch((error) => {
+      //   res.send(error);
+      // });   
     }  
     console.log(pro_details_arr);
     res.send(pro_details_arr);
