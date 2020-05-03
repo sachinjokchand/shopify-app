32799f0909973bfbc5c49e4750cddc05f38133b4
sachin jogchand <sachincodingkart@gmail.com>
2020-05-03 20:33:20 +0530

    initail commit

--- index.js
+++ index.js
@@ -276,101 +276,97 @@ app.post('/add-to-wish',(req, res) => {
     var shop_resp = [];
     var pro_obj = req.body.pro_arr;
     var pro_arr = JSON.parse(pro_obj);
-    var id_arr = {};
-    for (var i = 0; i < pro_arr.length; i++) {
-     id_arr[i] = pro_arr[0].product_id;
-    }
-    res.send(id_arr);
-    //   const shopRequestUrl_cust = 'https://' + req.body.shop_name + '/admin/api/2020-01/customers/'+req.body.cust_id+'.json';
-    //   const shopRequestHeaders_cust = {
-    //      'X-Shopify-Access-Token': accessToken,
-    //    };
+  
+      const shopRequestUrl_cust = 'https://' + req.body.shop_name + '/admin/api/2020-01/customers/'+req.body.cust_id+'.json';
+      const shopRequestHeaders_cust = {
+         'X-Shopify-Access-Token': accessToken,
+       };
 
-    //   global_req.get(shopRequestUrl_cust, { headers: shopRequestHeaders_cust })
-    //   .then((cust_response) => {
-    //        cust_resp = JSON.parse(cust_response);
-    //      })
-    //   .catch((error) => {
-    //     res.send(error);
-    //   });  
+      global_req.get(shopRequestUrl_cust, { headers: shopRequestHeaders_cust })
+      .then((cust_response) => {
+           cust_resp = JSON.parse(cust_response);
+         })
+      .catch((error) => {
+        res.send(error);
+      });  
     
-    //   for (var i = 0; i < pro_arr.length; i++) {
+      for (var i = 0; i < pro_arr.length; i++) {
         
-    //   const shopRequestUrl_prod = 'https://' + req.body.shop_name + '/admin/api/2020-04/products/'+pro_arr[i]+'.json';
-    //   const shopRequestHeaders_prod = {
-    //     'X-Shopify-Access-Token': accessToken,
-    //   };
+      const shopRequestUrl_prod = 'https://' + req.body.shop_name + '/admin/api/2020-04/products/'+pro_arr[0].product_id+'.json';
+      const shopRequestHeaders_prod = {
+        'X-Shopify-Access-Token': accessToken,
+      };
 
-    //   global_req.get(shopRequestUrl_prod, { headers: shopRequestHeaders_prod })
-    //   .then((shopResponse) => {
-    //   shop_resp = JSON.parse(shopResponse);
+      global_req.get(shopRequestUrl_prod, { headers: shopRequestHeaders_prod })
+      .then((shopResponse) => {
+      shop_resp = JSON.parse(shopResponse);
   
-    // var shop_name  = req.body.shop_name;
-    // var cust_id    = req.body.cust_id;
-    // var cust_name  = cust_resp.customer.first_name+' '+ cust_resp.customer.last_name;
-    // var cust_email = cust_resp.customer.email;
-    // var url        = shop_resp.product.title.replace(/\s+/g, '-').toLowerCase();
-    // var pro_url    = 'https://' + req.body.shop_name+'/products/'+url;
-    // var pro_price  =  req.body.p_currency+' '+shop_resp.product.variants[0].price;
-    // var pro_time   = new Date().toISOString();
+    var shop_name  = req.body.shop_name;
+    var cust_id    = req.body.cust_id;
+    var cust_name  = cust_resp.customer.first_name+' '+ cust_resp.customer.last_name;
+    var cust_email = cust_resp.customer.email;
+    var url        = shop_resp.product.title.replace(/\s+/g, '-').toLowerCase();
+    var pro_url    = 'https://' + req.body.shop_name+'/products/'+url;
+    var pro_price  =  req.body.p_currency+' '+shop_resp.product.variants[0].price;
+    var pro_time   = new Date().toISOString();
     
   
-    // var wish_list_data = {shop_name: shop_name, cust_id: cust_id };
-    // var cust_data = {shop_name: shop_name, cust_id: cust_id, cust_name: cust_name, cust_email: cust_email };
-    // var prod_data = {shop_name: shop_name, cust_id: cust_id, pro_id: shop_resp.product.id, pro_title: shop_resp.product.title, pro_img: shop_resp.product.image.src, pro_price: pro_price, pro_url: pro_url, pro_time: pro_time };
+    var wish_list_data = {shop_name: shop_name, cust_id: cust_id };
+    var cust_data = {shop_name: shop_name, cust_id: cust_id, cust_name: cust_name, cust_email: cust_email };
+    var prod_data = {shop_name: shop_name, cust_id: cust_id, pro_id: shop_resp.product.id, pro_title: shop_resp.product.title, pro_img: shop_resp.product.image.src, pro_price: pro_price, pro_url: pro_url, pro_time: pro_time };
 
-    //   let sql_cust = "SELECT * FROM user_data WHERE customer_id='"+cust_id+"' AND shop_name='"+shop_name+"'";
-    //   let query_pro = conn.query(sql_cust, (err, results) => {
+      let sql_cust = "SELECT * FROM user_data WHERE customer_id='"+cust_id+"' AND shop_name='"+shop_name+"'";
+      let query_pro = conn.query(sql_cust, (err, results) => {
     
-    //    if ( results.rows.length > 0 ) 
-    //         { console.log("user already exist.");  }
+       if ( results.rows.length > 0 ) 
+            { console.log("user already exist.");  }
           
-    //    else {  
-    //            const  query = {
-    //             text: 'INSERT INTO user_data(shop_name, customer_id, customer_name, customer_email ) VALUES($1, $2, $3, $4)',
-    //             values: [cust_data.shop_name, cust_data.cust_id, cust_data.cust_name, cust_data.cust_email ],
-    //            }
-    //            conn.query(query, (err, results) => {
-    //             if (err) { console.log("111"); } 
-    //             else { 
-    //                      const  query = {
-    //                             text: 'INSERT INTO wish_list(shop_name, customer_id ) VALUES($1, $2)',
-    //                             values: [wish_list_data.shop_name, wish_list_data.cust_id ],
-    //                            }
-    //                       conn.query(query, (err, results) => {
-    //                       if (err) { console.log("222"); } 
-    //                       else { }
-    //                     });
-    //                  }  
-    //            });
-    //          }
-    //           let sql_pro = "SELECT * FROM product_data WHERE customer_id='"+cust_id+"' AND product_id='"+prod_data.pro_id+"' AND shop_name='"+shop_name+"'";
-    //           let query_pro = conn.query(sql_pro, (err, results) => {
-    //            // var obj = {};
-    //            // obj['err'] = err;
-    //            //  obj['results'] = results;
-    //            //  res.send(obj);
-    //            if (  results.rows.length > 0  ) 
-    //            {  res.send(0); }
-    //            else{
-    //               const query = {
-    //                     text: 'INSERT INTO product_data(shop_name, customer_id, product_id,  product_title, product_src, product_price, product_url, product_time ) VALUES($1, $2, $3, $4, $5, $6, $7,$8)',
-    //                     values: [prod_data.shop_name, prod_data.cust_id, prod_data.pro_id, prod_data.pro_title, prod_data.pro_img, prod_data.pro_price, prod_data.pro_url, prod_data.pro_time ],
-    //                    }
-    //                    conn.query(query, (err, results) => {
-    //                     if (err) { res.send(err); } 
-    //                     else {
-    //                          res.send(1);
-    //                          }
-    //                    });                
-    //                }
-    //           });
-    //      });  
-    //   })
-    //   .catch((error) => {
-    //     res.send(error);
-    //   });   
-    // }  
+       else {  
+               const  query = {
+                text: 'INSERT INTO user_data(shop_name, customer_id, customer_name, customer_email ) VALUES($1, $2, $3, $4)',
+                values: [cust_data.shop_name, cust_data.cust_id, cust_data.cust_name, cust_data.cust_email ],
+               }
+               conn.query(query, (err, results) => {
+                if (err) { console.log("111"); } 
+                else { 
+                         const  query = {
+                                text: 'INSERT INTO wish_list(shop_name, customer_id ) VALUES($1, $2)',
+                                values: [wish_list_data.shop_name, wish_list_data.cust_id ],
+                               }
+                          conn.query(query, (err, results) => {
+                          if (err) { console.log("222"); } 
+                          else { }
+                        });
+                     }  
+               });
+             }
+              let sql_pro = "SELECT * FROM product_data WHERE customer_id='"+cust_id+"' AND product_id='"+prod_data.pro_id+"' AND shop_name='"+shop_name+"'";
+              let query_pro = conn.query(sql_pro, (err, results) => {
+               // var obj = {};
+               // obj['err'] = err;
+               //  obj['results'] = results;
+               //  res.send(obj);
+               if (  results.rows.length > 0  ) 
+               {  res.send(0); }
+               else{
+                  const query = {
+                        text: 'INSERT INTO product_data(shop_name, customer_id, product_id,  product_title, product_src, product_price, product_url, product_time ) VALUES($1, $2, $3, $4, $5, $6, $7,$8)',
+                        values: [prod_data.shop_name, prod_data.cust_id, prod_data.pro_id, prod_data.pro_title, prod_data.pro_img, prod_data.pro_price, prod_data.pro_url, prod_data.pro_time ],
+                       }
+                       conn.query(query, (err, results) => {
+                        if (err) { res.send(err); } 
+                        else {
+                             res.send(1);
+                             }
+                       });                
+                   }
+              });
+         });  
+      })
+      .catch((error) => {
+        res.send(error);
+      });   
+    }  
 });
 
 app.post('/remove_prod',(req, res) => {  
